c5b5ae266cc18d3926bad10bcdc792b83a4b7033
Bacika69 <horvathbator5@gmail.com>
2025-11-12 23:43:04 +0100

    Update urls.py

--- base/urls.py
+++ base/urls.py
@@ -1,11 +1,23 @@
 from django.urls import path
 from . import views
 from .views import SearchView
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
+import os
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('room/<int:pk>/', views.room, name="room"),
-    path('sneakerek/', views.sneakerek, name="sneakerek"),
-    path('search/', SearchView.as_view(), name='search'),
-    path('gyakorikerdesek/', views.gyk, name='gyakorikerdesek'),
-]
+    path("ads.txt", serve, {
+        'path': "ads.txt",
+        'document_root': os.path.join(settings.BASE_DIR, "templates"),
+        'show_indexes': False
+    }),
+    path('privacypolicy/', views.gyk, name='gyakorikerdesek'),
+    path('aboutus/', views.ab, name='aboutus'),
+    path('whatispsl/', views.wp, name='whatispsl'),
+    path('aboutlooksmaxxing/', views.looks, name='aboutlooksmaxxing'),
+    path('psychology-of-self-image/', views.pos, name='pos'),
+    path('contact/', views.contact, name='contact'),
+    path('terms-of-use/', views.term, name='term'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
